540839928cf50794cd55b47dcbac493d24ca4d13
XenkQ <77151114+XenkQ@users.noreply.github.com>
2024-01-28 20:48:24 +0100

    adding energy slide bar charging transition

--- Assets/Scripts/Cristal.cs
+++ Assets/Scripts/Cristal.cs
@@ -6,9 +6,6 @@ public class Cristal : MonoBehaviour, IInteractable, IEnergyHoldable
     [SerializeField] private EnergyContainer _energyContainer;
     public EnergyContainer EnergyContainer => _energyContainer;
 
-    public void Interact()
-    {
+    public void Interact() =>
         EnergySystem.ChannelEnnergyToPlayer(this, transform.position);
-        Debug.Log($"{this.transform.name} | {EnergyContainer.CurrentEnergy} | {EnergyContainer.MinEnergy}");
-    }
 }
--- Assets/Scripts/EnergySystem.cs
+++ Assets/Scripts/EnergySystem.cs
@@ -1,18 +1,22 @@
+using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class EnergySystem : MonoBehaviour, IEnergyHoldable
 {
     [Header("UI")]
     [SerializeField] private Slider _energySlider;
+    [SerializeField] private float _energyTransitionEffectSpeed = 4f;
     
     [Header("Energy Storage")]
     [SerializeField] private EnergyContainer _energyContainer;
     public EnergyContainer EnergyContainer => _energyContainer;
 
-    [Header("Energy Channeling")] [SerializeField]
-    private float _delayBetweenChanneling = 1;
+    [Header("Energy Channeling")]
+    [SerializeField] private float _delayBetweenChanneling = 1;
+    
     public static float EnergyAmountPerChannel = 15f;
     private static bool _isChannelingEnergy;
 
@@ -26,6 +30,16 @@ public class EnergySystem : MonoBehaviour, IEnergyHoldable
     private void Start()
     {
         _energySlider.maxValue = _energyContainer.MaxEnergy;
+    }
+
+    private void Update()
+    {
+        if (CanUpdateEnergySlider()) UpdateEnergySlider();
+    }
+
+    private bool CanUpdateEnergySlider()
+    {
+        return _energySlider.value != Instance.EnergyContainer.CurrentEnergy;
     }
 
     public static void ChannelEnnergyToPlayer(IEnergyHoldable energyHolder, Vector3 channelEffectPos)
@@ -44,7 +58,6 @@ public class EnergySystem : MonoBehaviour, IEnergyHoldable
             if (energyHolder.EnergyContainer.DecreaseEnergy(EnergyAmountPerChannel))
             {
                 Instance.EnergyContainer.IncreaseEnergy(EnergyAmountPerChannel);
-                Debug.Log($"Channeling... {Instance.EnergyContainer.CurrentEnergy}");
             }
             else
             {
@@ -59,5 +72,12 @@ public class EnergySystem : MonoBehaviour, IEnergyHoldable
         _isChannelingEnergy = false;
     }
 
-    private void UpdateEnergySlider() => _energySlider.value = Instance.EnergyContainer.CurrentEnergy;
+    private void UpdateEnergySlider()
+    {
+        _energySlider.value = Mathf.Lerp(
+            _energySlider.value,
+            Instance.EnergyContainer.CurrentEnergy,
+            _energyTransitionEffectSpeed * Time.deltaTime
+        );
+    }
 }
