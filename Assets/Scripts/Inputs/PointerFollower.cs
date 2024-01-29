32366dd555a563c29f309564c1aec0225ef4dff7
XenkQ <77151114+XenkQ@users.noreply.github.com>
2024-01-29 18:04:17 +0100

    adding camera following pointer object, changing folder hierarhy

---
+++ Assets/Scripts/Inputs/PointerFollower.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PointerFollower : MonoBehaviour
+{
+    [SerializeField] private float _maxDistanceFromTarget = 20f;
+    [SerializeField] private Transform _target;
+
+    private void FixedUpdate()
+    {
+        FollowPointerToMaxDistanceFromTarget(_maxDistanceFromTarget, _target);
+    }
+
+    private void FollowPointerToMaxDistanceFromTarget(float maxDistance, Transform target)
+    {
+        Vector3 pointerPos = Pointer.OnScreenWorldPosition;
+        Vector3 followerMaxPos = target.position + maxDistance * Vector3.one;
+
+        transform.position = new Vector3(
+            Mathf.Clamp(pointerPos.x, -followerMaxPos.x, followerMaxPos.x),
+            0,
+            Mathf.Clamp(pointerPos.z, -followerMaxPos.z, followerMaxPos.z)
+        );
+    }
+}
