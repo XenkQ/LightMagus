977e4db9e39d2174fa77521138ed58c7cc316d39
Konrad Blaszyński <blaszynski.konrad@gmail.com>
2024-02-29 21:38:22 +0100

    Changing camera follower class and assigning new layers

--- Assets/Scripts/Camera/CameraFollowPoint.cs
+++ Assets/Scripts/Camera/CameraFollowPoint.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class CameraFollowPoint : MonoBehaviour
 {
-    [SerializeField] private float _maxDistanceFromTarget = 20f;
+    [SerializeField] private Vector2 _maxDistanceFromTarget;
     [SerializeField] private Transform _target;
 
     private void LateUpdate()
@@ -11,15 +11,18 @@ public class CameraFollowPoint : MonoBehaviour
         FollowPointerToMaxDistanceFromTarget(_maxDistanceFromTarget, _target);
     }
 
-    private void FollowPointerToMaxDistanceFromTarget(float maxDistance, Transform target)
+    private void FollowPointerToMaxDistanceFromTarget(Vector2 maxDistance, Transform target)
     {
         Vector3 pointerPos = Pointer.OnScreenWorldPosition;
-        Vector3 followerMaxPos = target.position + maxDistance * Vector3.one;
+        float maxHorizontal = target.position.x + maxDistance.x;
+        float minHorizontal = target.position.x - maxDistance.x;
+        float maxVertical = target.position.y + maxDistance.y;
+        float minVertical = target.position.y - maxDistance.y;
 
         transform.position = new Vector3(
-            Mathf.Clamp(pointerPos.x, -followerMaxPos.x, followerMaxPos.x),
+            Mathf.Clamp(pointerPos.x, minHorizontal, maxHorizontal),
             0,
-            Mathf.Clamp(pointerPos.z, -followerMaxPos.z, followerMaxPos.z)
+            Mathf.Clamp(pointerPos.z, minVertical, maxVertical)
         );
     }
 }
